9472453159a30b6477e305f7fa8e41f5f62b7ae0
Dmirr <dmitriyrodionow@gmail.com>
2022-10-13 21:19:06 +0300

    added all files of project

---
+++ OK.js
@@ -0,0 +1,9 @@
+const sayHelloLinting = (fName) => {console.log(`Look, how pretty it is, ${fName}`);};
+
+sayHelloLinting('Oksana');
+
+const sayHelloLinting = (fName) => {
+  console.log(`Look, how pretty it is, ${fName}`);
+};
+
+sayHelloLinting("Oksana");
